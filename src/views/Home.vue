af14259531f316b054dfe0f159007eaf8624fa7f
Chetan Raj <chetan.narian@gmail.com>
2020-02-28 12:07:12 +0530

    Changes: Added Github Corner link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <GithubCorner />
     <Container msg="ES Explorer" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import GithubCorner from '@/components/GithubCorner.vue';
 import Container from '@/components/Container.vue';
 
 export default {
   name: 'Home',
   components: {
+    GithubCorner,
     Container,
   },
 };
